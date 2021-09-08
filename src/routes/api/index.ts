7556e0de4aaaa1b5bc3061c5dd09b630de6090f8
LEEJUN KIM <hwanam1111@naver.com>
2021-09-08 19:38:07 +0900

    Fix: login and sign up api move to auth router.

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import authRouter from './v1/auth';
 import userRouter from './v1/user';
 
 const app = express();
 
+app.use('/v1/auth', authRouter);
 app.use('/v1/users', userRouter);
 
 export default app;
---
+++ src/routes/api/v1/auth.ts
@@ -0,0 +1,80 @@
+import 'reflect-metadata';
+import express from 'express';
+import passport from 'passport';
+import { Container } from 'typedi';
+
+import logger from '../../../../config/winston';
+
+import AuthService from '../../../services/auth';
+
+const router = express.Router();
+
+router.post('/sign-up', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    const authServiceInstance = Container.get(AuthService);
+    const { httpStatusCode, data, message } = await authServiceInstance.signUpService(req.body);
+
+    return res.status(httpStatusCode).json({
+      data, message,
+    });
+  } catch (err) {
+    logger.error(`[${req.method}] '${req.originalUrl}'`, err);
+    return next(err);
+  }
+});
+
+router.post('/login', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    passport.authenticate('local', async (err: any, user: any, info: any) => {
+      if (err) {
+        logger.error(`[${req.method}] '${req.originalUrl}' - Auth error`, err);
+        return next(err);
+      }
+
+      const userAgent = req.headers['user-agent'];
+      const { email } = req.body;
+
+      const authServiceInstance = Container.get(AuthService);
+      if (info) {
+        await authServiceInstance.loginLogService({
+          email,
+          userAgent,
+          loginResult: false,
+          failureReason: info.reason,
+          classification: 'menual',
+        });
+
+        return res.status(401).json({
+          data: 'login-failure',
+          message: info.reason,
+        });
+      }
+
+      return req.login(user, async (loginError) => {
+        if (loginError) {
+          logger.error(`[${req.method}] '${req.originalUrl}' - Passport login error`, loginError);
+          return next(loginError);
+        }
+
+        await authServiceInstance.loginLogService({
+          email,
+          userAgent,
+          loginResult: true,
+          failureReason: '',
+          classification: 'menual',
+        });
+
+        return res.status(201).json({
+          data: 'login-success',
+          message: '로그인이 완료되었습니다.',
+        });
+      });
+    })(req, res, next);
+
+    return null;
+  } catch (err) {
+    return next(err);
+  }
+});
+
+export default router;
